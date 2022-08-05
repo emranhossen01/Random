130b03b47ead8f7adbba71a31a2a4442179f320d
Emran Hossen <92653363+emranhossen26@users.noreply.github.com>
2022-08-05 17:15:00 +0600

    Create 04.Range of the Circle.cpp

---
+++ 04.Range of the Circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double radious,range;
+
+    cin>>radious;
+
+    range=2*M_PI*radious;
+
+    cout<<"Range Of The Circle:"<<range;
+}
